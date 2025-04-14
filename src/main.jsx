1b1000c3cdd030722e58fb78d130cf090c520e1d
Jordan Schulte <email.jordanschulte@gmail.com>
2025-04-14 19:23:59 -0400

    Added variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import './CSS/base.css'
+import './CSS/variables.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
