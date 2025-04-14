56823b0a5d3a7439b753c0dafd3eca0ec3404cf2
Jordan Schulte <email.jordanschulte@gmail.com>
2025-04-14 15:19:59 -0400

    Organized file path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-
 function App() {
-
   return (
     <>
-      <h1 className='font-bold text-blue-600'>hello tailwind</h1>
+      <h1>hello world</h1>
     </>
   )
 }
---
+++ src/Comps/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer-wrapper'>
+        <p>Jordan Schulte 2025</p>
+    </div>
+  )
+}
+
+export default Footer
