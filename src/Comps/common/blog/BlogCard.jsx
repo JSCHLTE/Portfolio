d2f704a0609ed0bb215cc02634141706faa2b212
Jordan Schulte <email.jordanschulte@gmail.com>
2025-04-22 10:16:22 -0400

    Started styling blogs

--- src/Comps/common/blog/BlogCard.jsx
+++ src/Comps/common/blog/BlogCard.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 const BlogCard = ({ blog }) => {
   return (
     <article className='blog-item'>
-        <h3>{blog.title}</h3>
-        <p>{blog.snippet}</p>
+        <h3 className='blog-title'>{blog.title}</h3>
+        <p className='blog-snippet'>{blog.snippet}</p>
         <div className='blog-meta'>
             <div className='blog-meta-left'>
-                <img src="/images/pfp.webp" alt="Image of Jordan" />
-                <span>{blog.author}</span>
+              <img src="/images/pfp.webp" alt="Image of Jordan" className='blog-meta-pfp' />
+              <span className='blog-meta-author'>{blog.author}</span>
             </div>
             <div className='blog-meta-right'>
-                {blog.likes}
+              <i className="fa-regular fa-heart"></i>
+              <span className='blog-meta-likes'>{blog.likes}</span>
             </div>
         </div>
     </article>
