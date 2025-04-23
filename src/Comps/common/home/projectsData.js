34562211e7c5f340a0dbfce8b4c4f4e14bd5492c
Jordan Schulte <email.jordanschulte@gmail.com>
2025-04-23 18:18:07 -0400

    Added real project data and fixed some styling issues

--- src/Comps/common/home/projectsData.js
+++ src/Comps/common/home/projectsData.js
@@ -1,37 +1,37 @@
 export const projectsData = [
     {
         id: 0,
-        thumbnail: "/images/logo.png",
-        title: 'Test Project 1',
-        desc: 'This is a test project and this is also a placeholder paragraph',
-        tags: ['React', 'Open AI'],
-        github: 'https://github.com/JSCHLTE', //this will link to project repo later
-        live: 'https://github.com/JSCHLTE'
-    }, 
-    {
-        id: 1,
-        thumbnail: "/images/logo.png",
-        title: 'Test Project 2',
-        desc: 'This is a test project and this is also a placeholder paragraph',
+        thumbnail: "/images/portfolio.png",
+        title: 'Personal Portfolio',
+        desc: 'This is where I keep the projects I’ve built—stuff I’m proud of, lessons I’ve learned, and how I’ve grown as a developer.',
         tags: ['React'],
         github: 'https://github.com/JSCHLTE', //this will link to project repo later
         live: 'https://github.com/JSCHLTE'
     }, 
     {
+        id: 1,
+        thumbnail: "/images/golfcaddy.png",
+        title: 'Golf Caddy',
+        desc: 'A personal AI-powered golf caddy that helps you choose the right shot based on your lie and course conditions.',
+        tags: ['React', 'Open AI'],
+        github: 'https://github.com/JSCHLTE', //this will link to project repo later
+        live: 'https://github.com/JSCHLTE'
+    }, 
+    {
         id: 2,
-        thumbnail: "/images/logo.png",
-        title: 'Test Project 3',
-        desc: 'This is a test project and this is also a placeholder paragraph',
+        thumbnail: "/images/shoppingcart.png",
+        title: 'Shopping Cart',
+        desc: 'A Frontend Mentor challenge implemented in React, focused on building a fully functional shopping cart. Gained hands-on experience with useState, conditional rendering, and data flow across components.',
         tags: ['React'],
         github: 'https://github.com/JSCHLTE', //this will link to project repo later
         live: 'https://github.com/JSCHLTE'
     }, 
     {
         id: 3,
-        thumbnail: "/images/logo.png",
-        title: 'Test Project 4',
-        desc: 'This is a test project and this is also a placeholder paragraph',
-        tags: ['React', 'JavaScript'],
+        thumbnail: "/images/browserextensions.png",
+        title: 'Browser Extensions',
+        desc: 'My first Frontend Mentor challenge using React. It gave me a simple but solid introduction to how state and props work.',
+        tags: ['React'],
         github: 'https://github.com/JSCHLTE', //this will link to project repo later
         live: 'https://github.com/JSCHLTE'
     }, 
