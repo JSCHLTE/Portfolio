15ac4ab6a1ba345e3e944950d127b3ff4f0d1e2c
Jordan Schulte <email.jordanschulte@gmail.com>
2025-05-01 01:05:46 -0400

    Started login page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <h1 className='login-title'>Login</h1>
+        <form>
+            <input type="text" />
+        </form>
+    </>
+  )
+}
+
+export default Login
