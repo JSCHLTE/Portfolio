9e0d1fb2e734a9f3055fa467fb64dce896c7f8b8
Jordan Schulte <email.jordanschulte@gmail.com>
2025-05-01 17:54:56 -0400

    Now handling login forms in state

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,20 +1,37 @@
 import React, { useState } from 'react'
+import AnimatedText from '../Comps/common/AnimatedText'
 
 const Login = () => {
+
+  const [loginValues, setLoginValues] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setLoginValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
   return (
+
     <>
-        <h1 className='login-title'>Login</h1>
+        <h1 className='login-title'><AnimatedText text='Login'/></h1>
         <form id='loginForm'>
           <div className='username-wrapper'>
             <label htmlFor='username'>
               Username:
-              <input type="text" id='username' name='username'/>
+              <input type="text" id='username' name='username' onChange={handleChange} value={loginValues.username}/>
             </label>
           </div>
           <div className='password-wrapper'>
             <label htmlFor='password'>
               Password:
-              <input type="password" id='password' name='password'/>
+              <input type="password" id='password' name='password' onChange={handleChange} value={loginValues.password}/>
             </label>
           </div>
           <button className='login-button'>Login</button>
